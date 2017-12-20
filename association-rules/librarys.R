5ab606d992545efda0f82d57cba1db211728d09e
jadiazg02 <joseadiazg02@correo.ugr.es>
2017-12-20 20:24:16 +0100

    Empezado estudio práctico de Reglas de Asociación.

---
+++ association-rules/librarys.R
@@ -0,0 +1,12 @@
+#Librerías a instalar
+
+install.packages("arules")
+install.packages("arulesViz")
+install.packages("RKEEL")
+install.packages("pmml")
+
+
+library(arules)
+library(arulesViz)
+library(RKEEL)
+library(pmml)
